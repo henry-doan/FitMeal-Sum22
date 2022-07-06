246f7a86b5fc5e212bca9e088a383d773b5eb1f5
Heidi V <sharedmacc1@gmail.com>
2022-07-05 22:38:04 -0600

    Created auth pages & MainNavbar for user Login/Register

---
+++ client/src/components/shared/Nomatch.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const Nomatch = () => (
+  <>
+    <h1>404 -  Page Not Found</h1>
+    <Link to='/'>
+      Return Home
+    </Link>
+  </>
+)
+
+export default Nomatch;
+
