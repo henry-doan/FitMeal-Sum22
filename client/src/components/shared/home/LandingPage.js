75b1c0b6b8e4c8f30fecc47a9dc97b5f27ea5e3e
Hayley Mentz <hayley.mentz@gmail.com>
2022-07-21 10:41:55 -0700

    changed everything to bootstrap. Please lookover to see if the background image was done correctly

--- client/src/components/shared/home/LandingPage.js
+++ client/src/components/shared/home/LandingPage.js
@@ -1,59 +1,59 @@
 import { Link } from 'react-router-dom';
+import { Container, Row, Card, Col, Image, Footer} from 'react-bootstrap';
 
 const LandingPage = () => (
-<>
+ <>
 
-
+<Header>
 <Link to='/'>
   Return Home
-
 </Link>
-<link rel="stylesheet" href="style.css"></link>
-<div class="header">
-      <div class="container">
-        
-      </div>
-    </div>
+</Header>
 
-    <div class="jumbotron">
-      <div class="container">  
-        <div class="main">
-          <h1>FitMeal App Sum22</h1>
+      <Container>
+
+      <h1>FitMeal App Sum22</h1>
+    
+      <img class="background-image: url('https://unsplash.com/photos/0zkJ1EsH9dY')" />
+
+      <Row>
+  <Col>
+  <Card style={{ width: '18rem'}}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.body>
+    <Card.Title>Exercises</Card.Title>
+    <p>Find Popular Workouts</p>
+  </Card.body>
+</Card>
+</Col>   
           
 
-        </div>
-      </div>
-    </div>
+  <Col>
+  <Card style={{ width: '18rem'}}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.body>
+    <Card.Title>Workouts</Card.Title>
+    <p>Log Your Workouts!</p>
+  </Card.body>
+</Card>
+</Col>
+      
 
-    <div class="supporting">
-      <div class="container">
-        <div class="col">
-          <img src="https://unsplash.com/photos/0zkJ1EsH9dY"/>
-          <h2>Exercises</h2>
-          <p>Find popular exercises.</p>
-          <a class="btn-default" href="#">Learn more</a>
-        </div>
-        <div class="col">
-          <img src="https://unsplash.com/photos/0zkJ1EsH9dY"/>
-          <h2>Workouts</h2>
-          <p>Log workouts!</p>
-          <a class="btn-default" href="#">Learn more</a>
-        </div>
-        <div class="col">
-          <img src="https://unsplash.com/photos/0zkJ1EsH9dY"/>
-          <h2>Timer</h2>
-          <p>Use modern tools to get into shape.</p>
-          <a class="btn-default" href="#">Learn more</a>
-        </div>
-      </div>
-      <div class="clearfix"></div>
-    </div>
+  <Col>
+  <Card style={{ width: '18rem'}}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.body>
+    <Card.Title>Timer</Card.Title>
+    <p>Use Modern Tools to Get Into Shape!</p>
+  </Card.body>
+</Card>
+</Col>
+    </Row>
+</Container>
 
-    <div class="footer">
-      <div class="container">
-        <p>&copy; DevPoint Labs Part-time Sum 22 Cohort</p>
-      </div>
-    </div>
+<Footer>
+  <h3>DevPoint Labs Part-Time Summer 2022 Cohort</h3>
+</Footer>
 </>
 
 )
