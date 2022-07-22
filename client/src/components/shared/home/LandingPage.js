bb490ce957231bfd0863093b2fff4123d31cf8e3
Hayley Mentz <hayley.mentz@gmail.com>
2022-07-21 18:40:56 -0700

    Layed register page

--- client/src/components/shared/home/LandingPage.js
+++ client/src/components/shared/home/LandingPage.js
@@ -1,14 +1,19 @@
 import Login from '../../auth/Login';
-import { Row, Col, Image} from 'react-bootstrap';
+import { Row, Col, Image, Container} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+
 
 const LandingPage = () => (
 <>
   <Row>
     <Col md={6}>
-      <Login />
-      <footer>
-        <h6>© 2022 DevPoint Labs Part-Time Summer Cohort</h6>
-      </footer>
+      <Container>
+        <Login />
+        <p>Don't Have an Account? <Link to='/register'>Sign Up</Link></p>
+        <footer>
+          <h6>© 2022 DevPoint Labs Part-Time Summer Cohort</h6>
+        </footer>
+      </Container>
     </Col>
     <Col md={6}>
       <Image src="https://images.unsplash.com/photo-1603077492340-e6e62b2a688b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
