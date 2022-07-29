b7651dfa971bb55c3ba42288c37e6dab10d766e3
Henry Doan <henrydoan96@gmail.com>
2022-07-28 19:09:09 -0600

    Update WorkoutShow.js

--- client/src/components/workout/WorkoutShow.js
+++ client/src/components/workout/WorkoutShow.js
@@ -65,7 +65,7 @@ const WorkoutShow = ({
 
       <Button
       variant="secondary"
-      onClick={() => addUserWorkout(workoutId, user.id)}
+      onClick={() => addUserWorkout(user.id, workoutId)}
       >
       Add to My workout
       </Button>
