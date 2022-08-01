35a614aeae29c19ee7c1e4f0968b73250ba06b6f
Hayley Mentz <hayley.mentz@gmail.com>
2022-08-01 14:02:33 -0700

    adding styling

--- client/src/components/shared/Team.js
+++ client/src/components/shared/Team.js
@@ -1,18 +1,21 @@
 import {Col, Row, Card, Button, Stack, Image, CDN} from 'react-bootstrap';
 import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai';
+import {TeamTitle} from './../styles/teampage';
+
+
 
 const Team = () => (
 
 <>
-<h1 class="text-center">Meet our Team</h1>
+<TeamTitle>Meet our Team</TeamTitle>
 
 
 
- <Row md ={1} xl={5}>
+ <Row className='row-cols-1 row-cols-md-2 g-4'>
   
     <Col md={2.4} >
-      <Card border ='primary' style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="./images/image1.jpg" />
+      <Card style={{ width: '18rem' }}>
+     <Card.Img variant="top" src="./images/image1.jpg" />
       <Card.Body>
         <Card.Title>Brian Kershisnik</Card.Title>
         <Card.Text>
@@ -25,23 +28,22 @@ const Team = () => (
     </Col>
   
     <Col md={2.4} >
-      <Card border = 'info' style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card  style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="./images/image2.jpg" />
       <Card.Body>
         <Card.Title>Anna Li</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Former musician turned Junior Developer.
         </Card.Text>
         <Card.Link href="https://github.com/amazingannali"><AiFillGithub /></Card.Link>
-        <Card.Link href="#"><AiFillLinkedin /></Card.Link>
-        <Card.Link href="mailto:"><AiOutlineMail /></Card.Link>
+        <Card.Link href="https://www.linkedin.com/in/anna-li-93bb3926/"><AiFillLinkedin /></Card.Link>
+        <Card.Link href="mailto:annadan2005@yahoo.com"><AiOutlineMail /></Card.Link>
       </Card.Body>
       </Card>
     </Col>
     
     <Col md={2.4}>
-      <Card border ='primary' style={{ width: '18rem' }}>
+      <Card  style={{ width: '18rem' }}>
       <Card.Img variant="top" src="./images/image0.jpeg" />
       <Card.Body>
         <Card.Title>Hayley Mentz</Card.Title>
@@ -56,7 +58,7 @@ const Team = () => (
     </Col>
     
     <Col md={2.4} >
-      <Card border = 'info' style={{ width: '18rem' }}>
+      <Card  style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Abdirahman Mohamed</Card.Title>
@@ -72,7 +74,7 @@ const Team = () => (
     </Col>
 
     <Col md={2.4}>
-      <Card border = 'primary' style={{ width: '18rem', margintop: '20px' }}>
+      <Card  style={{ width: '18rem', margintop: '20px' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Heidi Villa</Card.Title>
---
+++ client/src/components/styles/teampage.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const TeamTitle = styled.h1 `
+color: green;
+font-size: 52 pixels;
+text-align: center;
+`
+
