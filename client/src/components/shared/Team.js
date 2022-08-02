d6d00cc62a0269a48fd7e91fd5173446bc77121f
Hayley Mentz <hayley.mentz@gmail.com>
2022-08-01 18:19:29 -0700

    team styling

--- client/src/components/shared/Team.js
+++ client/src/components/shared/Team.js
@@ -1,101 +1,84 @@
-import {Col, Row, Card, Button, Stack, Image, CDN} from 'react-bootstrap';
+import {Col, Row, Card, Container } from 'react-bootstrap';
 import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai';
-import {TeamTitle} from './../styles/teampage';
-
-
+import {TeamTitle, TeamCard, TeamLink, TeamImage} from './../styles/teampage';
 
 const Team = () => (
-
-<>
-<TeamTitle>Meet our Team</TeamTitle>
-
-
-
- <Row className='row-cols-1 row-cols-md-2 g-4'>
-  
-    <Col md={2.4} >
-      <Card style={{ width: '18rem' }}>
-     <Card.Img variant="top" src="./images/image1.jpg" />
-      <Card.Body>
-        <Card.Title>Brian Kershisnik</Card.Title>
-        <Card.Text>
-        Brian Kershisnik is a full stack developer with a background in system administration and security.
-        </Card.Text>
-        <Card.Link href="https://github.com/briankershisnik"><AiFillGithub /></Card.Link>
-        <Card.Link href="mailto:bkershis@gmail.com"><AiOutlineMail /></Card.Link>
-      </Card.Body>
-      </Card>
+<Container>
+  <TeamTitle>Meet our Team</TeamTitle>
+  <Row xs={1} md={3} xl={5} className="g-5 justify-content-center">
+    <Col>
+      <TeamCard >
+        <TeamImage variant="top" src="./images/image1.jpg" />
+        <Card.Body>
+          <Card.Title>Brian Kershisnik</Card.Title>
+          <TeamLink href="https://github.com/briankershisnik"><AiFillGithub /></TeamLink>
+          <TeamLink href="mailto:bkershis@gmail.com"><AiOutlineMail /></TeamLink>
+          <Card.Text>
+          Brian Kershisnik is a full stack developer with a background in system administration and security.
+          </Card.Text>
+        </Card.Body>
+      </TeamCard>
     </Col>
-  
-    <Col md={2.4} >
-      <Card  style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="./images/image2.jpg" />
-      <Card.Body>
-        <Card.Title>Anna Li</Card.Title>
-        <Card.Text>
-        Former musician turned Junior Developer.
-        </Card.Text>
-        <Card.Link href="https://github.com/amazingannali"><AiFillGithub /></Card.Link>
-        <Card.Link href="https://www.linkedin.com/in/anna-li-93bb3926/"><AiFillLinkedin /></Card.Link>
-        <Card.Link href="mailto:annadan2005@yahoo.com"><AiOutlineMail /></Card.Link>
-      </Card.Body>
-      </Card>
+
+    <Col>
+      <TeamCard>
+        <TeamImage variant="top" src="./images/image2.jpg" />
+        <Card.Body>
+          <Card.Title>Anna Li</Card.Title>
+          <TeamLink href="https://github.com/amazingannali"><AiFillGithub /></TeamLink>
+          <TeamLink href="https://www.linkedin.com/in/anna-li-93bb3926/"><AiFillLinkedin /></TeamLink>
+          <TeamLink href="mailto:annadan2005@yahoo.com"><AiOutlineMail /></TeamLink>
+          <Card.Text>
+          Former musician turned Junior Developer.
+          </Card.Text>
+        </Card.Body>
+      </TeamCard>
     </Col>
-    
-    <Col md={2.4}>
-      <Card  style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="./images/image0.jpeg" />
-      <Card.Body>
-        <Card.Title>Hayley Mentz</Card.Title>
-        <Card.Text>
+
+    <Col>
+      <TeamCard>
+        <TeamImage variant="top" src="./images/image0.jpeg" />
+        <Card.Body>
+          <Card.Title>Hayley Mentz</Card.Title>
+          <TeamLink href="https://github.com/HayleyMentz"><AiFillGithub /></TeamLink>
+          <TeamLink href="https://www.linkedin.com/in/hayley-mentz-75713219/"><AiFillLinkedin /></TeamLink>
+          <TeamLink href="mailto:hayley.mentz@gmail.com"><AiOutlineMail /></TeamLink>
+          <Card.Text>
           Former Research Scientist turned Junior Developer.
-        </Card.Text>
-        <Card.Link href="https://github.com/HayleyMentz"><AiFillGithub /></Card.Link>
-        <Card.Link href="https://www.linkedin.com/in/hayley-mentz-75713219/"><AiFillLinkedin /></Card.Link>
-        <Card.Link href="mailto:hayley.mentz@gmail.com"><AiOutlineMail /></Card.Link>
-      </Card.Body>
-      </Card>
-    </Col>
-    
-    <Col md={2.4} >
-      <Card  style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Abdirahman Mohamed</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="https://github.com/ramgithup"><AiFillGithub /></Card.Link>
-        <Card.Link href="#"><AiFillLinkedin /></Card.Link>
-        <Card.Link href="mailto:"><AiOutlineMail /></Card.Link>
-      </Card.Body>
-      </Card>
+          </Card.Text>
+        </Card.Body>
+      </TeamCard>
     </Col>
 
-    <Col md={2.4}>
-      <Card  style={{ width: '18rem', margintop: '20px' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Heidi Villa</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="https://github.com/heidiv5689"><AiFillGithub /></Card.Link>
-        <Card.Link href="#"><AiFillLinkedin /></Card.Link>
-        <Card.Link href="mailto:"><AiOutlineMail /></Card.Link>
-      </Card.Body>
-      </Card>
+    <Col>
+      <TeamCard>
+        <TeamImage variant="top" src="" />
+        <Card.Body>
+          <Card.Title>Abdirahman Mohamed</Card.Title>
+          <TeamLink href="https://github.com/briankershisnik"><AiFillGithub /></TeamLink>
+          <TeamLink href="mailto:bkershis@gmail.com"><AiOutlineMail /></TeamLink>
+          <Card.Text>
+          Brian Kershisnik is a full stack developer with a background in system administration and security.
+          </Card.Text>
+        </Card.Body>
+      </TeamCard>
+    </Col>
+
+    <Col>
+      <TeamCard>
+        <TeamImage variant="top" src="" />
+        <Card.Body>
+          <Card.Title>Heidi Villa</Card.Title>
+          <TeamLink href="https://github.com/briankershisnik"><AiFillGithub /></TeamLink>
+          <TeamLink href="mailto:bkershis@gmail.com"><AiOutlineMail /></TeamLink>
+          <Card.Text>
+          Brian Kershisnik is a full stack developer with a background in system administration and security.
+          </Card.Text>
+        </Card.Body>
+      </TeamCard>
     </Col>  
-</Row>
- 
-   
-        
-</>
-
-
-
+  </Row>       
+</Container>
 )
 
 export default Team;
--- client/src/components/styles/teampage.js
+++ client/src/components/styles/teampage.js
@@ -1,8 +1,26 @@
+import { Card } from 'react-bootstrap';
 import styled from 'styled-components';
 
 export const TeamTitle = styled.h1 `
-color: green;
+color: #084C26;
 font-size: 52 pixels;
+font-family: 'roboto';
 text-align: center;
 `
 
+export const TeamCard = styled(Card)`
+color: #084C26;
+font-family: 'roboto';
+text-align: center;
+height: 500px; 
+width: 200px;
+`
+
+export const TeamLink = styled(Card.Link)`
+color: #EF7E6D;
+text-decoration: none;
+`
+
+export const TeamImage = styled(Card.Img)`
+height: 275px;
+`
