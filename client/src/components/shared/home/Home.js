1877c7fef9f2d83ffdbb5bce3c6acffbea15251f
anna li <annadan2005@yahoo.com>
2022-08-02 19:21:37 -0600

    newest and popular workouts

--- client/src/components/shared/home/Home.js
+++ client/src/components/shared/home/Home.js
@@ -1,8 +1,30 @@
-const Home = () => (
-  <>
-   <h1>
-    Home Page
-   </h1>
-  </>
-)
-export default Home;
+import { useEffect } from "react";
+import { WorkoutConsumer } from "../../../providers/WorkoutProvider";
+import ShowList from "./ShowList";
+
+const Home = ({
+  getPopularWorkouts,
+  popularWorkouts,
+  getNewestWorkouts,
+  newestWorkouts,
+}) => {
+  useEffect(() => {
+    getPopularWorkouts();
+    getNewestWorkouts();
+  }, []);
+
+  return (
+    <>
+      <h1>POPULAR WORKOUTS</h1>
+      <ShowList workouts={popularWorkouts} />
+
+      <h1>NEWEST WORKOUTS</h1>
+      <ShowList workouts={newestWorkouts} />
+    </>
+  );
+};
+
+const ConnectedWorkouts = (props) => (
+  <WorkoutConsumer>{(value) => <Home {...props} {...value} />}</WorkoutConsumer>
+);
+export default ConnectedWorkouts;
--- client/src/providers/WorkoutProvider.js
+++ client/src/providers/WorkoutProvider.js
@@ -1,133 +1,173 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export const WorkoutContext = React.createContext()
+export const WorkoutContext = React.createContext();
 
 export const WorkoutConsumer = WorkoutContext.Consumer;
 
 const WorkoutProvider = ({ children }) => {
-  const [workouts, setWorkouts] = useState([])
-  const [allWorkouts, setAllWorkouts] = useState([])
-  const [pagination, setPagination] = useState(1)
-  const [headers, setHeaders] = useState({})
-  const [errors, setErrors] = useState(null)
+  const [workouts, setWorkouts] = useState([]);
+  const [allWorkouts, setAllWorkouts] = useState([]);
+  const [pagination, setPagination] = useState(1);
+  const [headers, setHeaders] = useState({});
+  const [errors, setErrors] = useState(null);
 
-  const navigate = useNavigate()
-
+  const [popularWorkouts, setPopularWorkouts] = useState([]);
+  const [newestWorkouts, setNewestWorkouts] = useState([]);
+  const navigate = useNavigate();
 
   const getAllWorkouts = () => {
-    axios.get(`/api/workout_all`)
-    .then(res => {
-      const { data } = res 
-      setAllWorkouts(data)
-    })
-    .catch( err => {
-      console.log(err)
-      setErrors({
-        variant: 'danger',
-        msg: err.response.statusText
+    axios
+      .get(`/api/workout_all`)
+      .then((res) => {
+        const { data } = res;
+        setAllWorkouts(data);
       })
-    })
-  }
+      .catch((err) => {
+        console.log(err);
+        setErrors({
+          variant: "danger",
+          msg: err.response.statusText,
+        });
+      });
+  };
 
   const getWorkouts = (page = 1) => {
-    axios.get(`/api/workouts?page=${page}`)
-    .then( res => {
-      const { headers, data } = res 
-      const totalPages = Math.ceil(parseInt(headers['total']) / parseInt(headers['per-page']))
-      setPagination(totalPages)
-      setWorkouts(data)
-      setHeaders(headers)
-    })
-      .catch( err => {
-        console.log(err)
-        setErrors({
-          variant: 'danger',
-          msg: err.response.statusText
-        })
+    axios
+      .get(`/api/workouts?page=${page}`)
+      .then((res) => {
+        const { headers, data } = res;
+        const totalPages = Math.ceil(
+          parseInt(headers["total"]) / parseInt(headers["per-page"])
+        );
+        setPagination(totalPages);
+        setWorkouts(data);
+        setHeaders(headers);
       })
-  }
+      .catch((err) => {
+        console.log(err);
+        setErrors({
+          variant: "danger",
+          msg: err.response.statusText,
+        });
+      });
+  };
 
   const addWorkout = (workout) => {
     let data = new FormData();
-    data.append('file', workout.wimage);
-    data.append('wname', workout.wname);
-    data.append('difficulty', workout.difficulty);
-    axios.post('/api/workouts', data )
-      .then( res => setWorkouts([...workouts, res.data ]))
-      .catch( err => {
-        console.log(err)
-        let field = Object.keys(err.response.data.errors)[0]
-        let errMsg = Object.values(err.response.data.errors)[0]
+    data.append("file", workout.wimage);
+    data.append("wname", workout.wname);
+    data.append("difficulty", workout.difficulty);
+    axios
+      .post("/api/workouts", data)
+      .then((res) => setWorkouts([...workouts, res.data]))
+      .catch((err) => {
+        console.log(err);
+        let field = Object.keys(err.response.data.errors)[0];
+        let errMsg = Object.values(err.response.data.errors)[0];
         setErrors({
-          variant: 'danger',
-          msg: `${field} ${errMsg}`
-        })
-      })
-  }
+          variant: "danger",
+          msg: `${field} ${errMsg}`,
+        });
+      });
+  };
 
   const updateWorkout = (id, workout) => {
     let data = new FormData();
-    data.append('file', workout.wimage);
-    data.append('wname', workout.wname);
-    data.append('difficulty', workout.difficulty);
-    axios.put(`/api/workouts/${id}`, data )
-      .then( res => {
-        const newUpdateWorkouts = workouts.map( c => {
+    data.append("file", workout.wimage);
+    data.append("wname", workout.wname);
+    data.append("difficulty", workout.difficulty);
+    axios
+      .put(`/api/workouts/${id}`, data)
+      .then((res) => {
+        const newUpdateWorkouts = workouts.map((c) => {
           if (c.id === id) {
-            return res.data
+            return res.data;
           }
-          return c
-        })
-        setWorkouts(newUpdateWorkouts)
-        navigate('/workouts')
+          return c;
+        });
+        setWorkouts(newUpdateWorkouts);
+        navigate("/workouts");
       })
-      .catch( err => {
-        console.log(err)
-        let field = Object.keys(err.response.data.errors)[0]
-        let errMsg = Object.values(err.response.data.errors)[0]
+      .catch((err) => {
+        console.log(err);
+        let field = Object.keys(err.response.data.errors)[0];
+        let errMsg = Object.values(err.response.data.errors)[0];
         setErrors({
-          variant: 'danger',
-          msg: `${field} ${errMsg}`
-        })
-      })
-  }
+          variant: "danger",
+          msg: `${field} ${errMsg}`,
+        });
+      });
+  };
 
-  
   const deleteWorkout = (id) => {
-    axios.delete(`/api/workouts/${id}`)
-      .then( res => {
-        setWorkouts(workouts.filter( c => c.id !== id ))
-        navigate('/workouts')
+    axios
+      .delete(`/api/workouts/${id}`)
+      .then((res) => {
+        setWorkouts(workouts.filter((c) => c.id !== id));
+        navigate("/workouts");
       })
-      .catch( err => {
-        console.log(err)
+      .catch((err) => {
+        console.log(err);
         setErrors({
-          variant: 'danger',
-          msg: err.response.statusText
-        })
+          variant: "danger",
+          msg: err.response.statusText,
+        });
+      });
+  };
+
+  const getPopularWorkouts = () => {
+    axios
+      .get("/api/popularWorkouts")
+      .then((res) => setPopularWorkouts(res.data))
+      .catch((err) => {
+        console.log(err);
+        setErrors({
+          variant: "danger",
+          msg: err.response.statusText,
+        });
+      });
+  };
+
+  const getNewestWorkouts = () => {
+    axios
+      .get("/api/newestWorkouts")
+      .then((res) => {
+        setNewestWorkouts(res.data);
       })
-  }
+      .catch((err) => {
+        console.log(err);
+        setErrors({
+          variant: "danger",
+          msg: err.response.statusText,
+        });
+      });
+  };
 
   return (
-    <WorkoutContext.Provider value={{
-      workouts,
-      allWorkouts,
-      pagination,
-      headers,
-      errors,
-      setErrors,
-      getWorkouts,
-      getAllWorkouts,
-    
-      addWorkout,
-      updateWorkout,
-      deleteWorkout,
-    }}>
-      { children }
+    <WorkoutContext.Provider
+      value={{
+        workouts,
+        allWorkouts,
+        pagination,
+        headers,
+        errors,
+        setErrors,
+        getWorkouts,
+        getAllWorkouts,
+        addWorkout,
+        updateWorkout,
+        deleteWorkout,
+        getPopularWorkouts,
+        popularWorkouts,
+        getNewestWorkouts,
+        newestWorkouts,
+      }}
+    >
+      {children}
     </WorkoutContext.Provider>
-  )
-}
+  );
+};
 
 export default WorkoutProvider;
